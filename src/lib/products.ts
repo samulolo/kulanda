7ffe5f642081f73a7f7c58dce6a93e154894c58f
Samulolo Júnior <eliseusamulolo@gmail.com>
2026-08-11 00:41:44 +0100

    test price added

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -184,6 +184,8 @@ export const getCategories = cache(async function getCategories(): Promise<
 });
 
 
+
+
 export async function getProductBySlug(slug: string): Promise<Product | undefined> {
   const products = await getAllProducts();
   return products.find((p) => p.slug === slug);
